143bf0827df713c9d17443d2a07d0bbf785751dd
Bo Pang <brucepang96@gmail.com>
2018-03-12 13:35:11 -0700

    first commit

---
+++ model1.py
@@ -0,0 +1,15 @@
+class Model_One(object):
+	"""docstring for Model_One"""
+	def __init__(self, iter):
+		self.iter = iter
+		self.count = defaultdict(int)
+		self.bigram_count = defaultdict(int)
+		self.translation = dict()
+		
+
+	def delta(self,k,i,j):
+		pass
+
+
+	def train(self):
+		pass
